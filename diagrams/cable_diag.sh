8da9b28d343444c86f56b9653bd817bbd76684c8
Nathan Hui <ntlhui@gmail.com>
2020-11-07 19:40:16 -0800

    Ignoring generated files, adding power and cabling, added NIC

---
+++ diagrams/cable_diag.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+LUALATEX=lualatex
+PDF2SVG=pdf2svg
+RM=rm
+TARGET=cable_diag
+INKSCAPE=inkscape
+$LUALATEX $TARGET.tex
+$RM $TARGET.log
+$RM $TARGET.aux
+$PDF2SVG $TARGET.pdf $TARGET.svg
+$INKSCAPE --export-dpi=300 --export-png $TARGET.png $TARGET.svg
